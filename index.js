e3cc0ac1e04bddd0f22d991fbf351005c4236d02
xudargabovqaxramon-creator <xudargabovqaxramon@gmail.com>
2025-12-15 19:17:43 +0500

    added

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const cors = require("cors")
 const userRouter = require("./router/users.routes")
 const productsRouter = require("./router/products.routes")
 const adminsRouter = require("./router/admins.routes")
-
-
+const multer = require("multer")
+const path = require("path")
 
 const k_app = express()
 k_app.use(cors())
@@ -17,6 +17,117 @@ k_app.get("/", (req, res) => {
         message : " salom ustoz"
     })
 })
+
+
+
+
+
+//   LANDSHAFT
+const storage = multer.diskStorage({
+  destination: "./upload/landshaft",
+  filename: (req, file, cb) => {
+    const uniqueName = file.fieldname + "_" + Date.now()
+    const ext = path.extname(file.originalname)
+    cb(null, `${uniqueName}${ext}`)
+  }
+})
+
+const upload = multer({ storage })
+
+k_app.use("/landshaft", express.static("upload/landshaft"))
+
+k_app.post("/landshaft", upload.single("file"), (req, res) => {
+  return res.status(201).json({
+    filePath: "http://localhost:4001/landshaft/" + req.file.filename
+  })
+})
+
+
+
+// =_----- osimliklar--------
+
+
+
+const storagePlants = multer.diskStorage({
+  destination: "./upload/plants",
+  filename: (req, file, cb) => {
+    const uniqueName = file.fieldname + "_" + Date.now()
+    const ext = path.extname(file.originalname)
+    cb(null, `${uniqueName}${ext}`)
+  }
+})
+
+const upload1 = multer({ storage:storagePlants })
+
+k_app.use("/plants", express.static("upload/plants"))
+
+k_app.post("/plants", upload1.single("file"), (req, res) => {
+  return res.status(201).json({
+    filePath: "http://localhost:4001/plants/" + req.file.filename
+  })
+})
+
+
+// -----------hayvonlar------------
+
+
+
+
+
+const storageAnimals = multer.diskStorage({
+  destination: "./upload/animal",
+  filename: (req, file, cb) => {
+    const uniqueName = file.fieldname + "_" + Date.now()
+    const ext = path.extname(file.originalname)
+    cb(null, `${uniqueName}${ext}`)
+  }
+})
+
+const upload2 = multer({ storage:storageAnimals })
+
+k_app.use("/animal", express.static("upload/animal"))
+
+k_app.post("/animal", upload2.single("file"), (req, res) => {
+  return res.status(201).json({
+    filePath: "http://localhost:4001/animal/" + req.file.filename
+  })
+})
+
+
+
+
+// ---------- kun botishi -------------
+
+
+
+
+
+
+
+const storagesunrise= multer.diskStorage({
+  destination: "./upload/sunrise",
+  filename: (req, file, cb) => {
+    const uniqueName = file.fieldname + "_" + Date.now()
+    const ext = path.extname(file.originalname)
+    cb(null, `${uniqueName}${ext}`)
+  }
+})
+
+const upload3 = multer({ storage:storagesunrise })
+
+k_app.use("/sunrise", express.static("upload/sunrise"))
+
+k_app.post("/sunrise", upload3.single("file"), (req, res) => {
+  return res.status(201).json({
+    filePath: "http://localhost:4001/sunrise/" + req.file.filename
+  })
+})
+
+
+
+
+
+
 
 
 // Routers
